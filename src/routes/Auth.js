2d8cd27c1607d5ba7d695194faff86698ec45d19
Jiny <jiny8510@gmail.com>
2022-08-25 23:35:13 +0900

    login 다시보기

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -4,6 +4,7 @@ import fbase from "../fbase";
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(false);
   const onChange = (e) => {
     const {
       target: { name, value },
@@ -16,6 +17,11 @@ const Auth = () => {
   };
   const onSubmit = (e) => {
     e.preventDefault();
+    if (newAccount) {
+      //create account
+    } else {
+      //log in
+    }
   };
 
   return (
@@ -23,7 +29,7 @@ const Auth = () => {
       <form onSubmit={onSubmit}>
         <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
         <input name="password" type="password" placeholder="password" required value={password} onChange={onChange} />
-        <input type="submit" value="Log In" />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
       </form>
       <div>
         <button>Continue with Google</button>
