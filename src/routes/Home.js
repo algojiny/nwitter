2ee8c3d372af5e24f2516548da0aae9c97fa340a
Jiny <jiny8510@gmail.com>
2022-08-29 18:30:21 +0900

    진행중

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,25 @@
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-import React, { useState } from "react";
+import { async } from "@firebase/util";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  serverTimestamp,
+} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { db } from "../fbase";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  const getNweets = async () => {
+    const dbNweets = await getDocs(collection(db, "nweets"));
+    dbNweets.forEach((doc) => {
+      console.log(doc.data());
+    });
+  };
+  useEffect(() => {
+    getNweets();
+  }, []);
   const onSubmit = async (e) => {
     e.preventDefault();
     if (nweet !== "")
