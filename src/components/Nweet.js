d4d59b0674d766c040b77e1be766dc237c7dba8f
Jiny <jiny8510@gmail.com>
2022-08-30 11:55:54 +0900

    #3.5 Delete and Update part Two (11:12)

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,13 +1,55 @@
-import React from "react";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import React, { useState } from "react";
+import { db } from "../fbase";
 
 const Nweet = ({ nweetObj, isOwner }) => {
+  const [edting, setEditing] = useState(false); //edit모드 설정
+  const [newNweet, setNewNweet] = useState(nweetObj.text); //input에 입력된 text업데이트
+  const onDeleteClick = async () => {
+    const ok = confirm("Are you sure you want to delete this nweet?");
+    if (ok) {
+      await deleteDoc(doc(db, "nweets", nweetObj.id));
+    }
+  };
+  const toggleEditing = () => setEditing((prev) => !prev);
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await updateDoc(doc(db, "nweets", nweetObj.id), {
+      text: newNweet,
+    });
+    setEditing(false);
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewNweet(value);
+  };
   return (
     <div>
-      <h4>{nweetObj.text}</h4>
-      {isOwner && (
+      {edting ? (
         <>
-          <button>Delete Nweet</button>
-          <button>Edit Nweet</button>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Edit your nweet"
+              value={newNweet}
+              required
+              onChange={onChange}
+            />
+            <input type="submit" value="Update Nweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{nweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Nweet</button>
+              <button onClick={toggleEditing}>Edit Nweet</button>
+            </>
+          )}
         </>
       )}
     </div>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,6 @@
 import {
   addDoc,
   collection,
-  getDocs,
   onSnapshot,
   orderBy,
   query,
